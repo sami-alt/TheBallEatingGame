0832c591d253c8079132e1dfc740328ffbe464f7
Sami <goodfellatoo@gmail.com>
2020-03-12 14:23:44 +0200

    commit

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ function setup() {
 
   background(255, 255, 255);
  
+ 
+ 
+
  stroke(0);
- for (var i = 0; i < 0 ; i++) {
+
+ for (var i = 0; i < 2 ; i++){
    var X = 133 +(i*133);
    line(X, 0, X, 400);
-   for(var j = 0; j < 0; j++){
+   for(var j = 0; j < 2; j++){
      var Y = 133+(j*133);
+     line(0,Y , 400, Y);
    }
  }
 
-  
+ var k = 0;
+ 
+  mouseClicked = function() {
+    
+    if(k % 2) {
+      fill(255,0,0);
+      textSize(100);
+      text("0", mouseX - 35, mouseY + 35 );
+    } else {
+      fill(0,0,0);
+      textSize(100);
+      text("X", mouseX - 35, mouseY + 35);
+    }
+    k += 1;
+  }
 
 
 
